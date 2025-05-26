8c5041289e68cd73932a97e8fbbe08e6b1244743
sethome2 <1095646418@qq.com>
2025-05-27 02:49:35 +0800

    Finished Dockerfile for the project

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "run project star...."
 
+# force kill any process using port 8501 which is used by streamlit
+fuser -k 8501/tcp
+
+echo "run project star...."
 
 streamlit run ./code/main.py
 
